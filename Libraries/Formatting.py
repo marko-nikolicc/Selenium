51c808cbe70e55f8ef8af27c39cc583cd170ebb5
marko-nikolicc <marepl91@gmail.com>
2020-04-27 20:22:32 +0200

    Update Libraries.Formating.py

--- Libraries/Formatting.py
+++ Libraries/Formatting.py
@@ -2,43 +2,39 @@ import os
 from openpyxl import load_workbook
 from Libraries.Styles import *
 
-class Format():
+class Format_TestResultsForFacebookLogin():
 
     def __init__(self,file):
         self.file = file
 
     def formatTable(self):
         filePath = os.path.join(os.getcwd(), self.file)
-        UserPass = load_workbook(filePath)
+        DataAndResults = load_workbook(filePath)
 
-        Sheet1 = UserPass["login"]
-        Sheet3 = UserPass["test_login_results"]
+        Sheet1 = DataAndResults["DataForFacebookLogin"]
 
-        number = 1
-        for value in Sheet3.iter_rows(min_row=1, min_col=1, max_col=4, values_only=True):
-            Sheet3["A" + str(number)] = ""
-            Sheet3["B" + str(number)] = ""
-            Sheet3["C" + str(number)] = ""
-            Sheet3["D" + str(number)] = ""
-            number += 1
+        delete = DataAndResults.get_sheet_by_name("TestResultsForFacebookLogin")
+        DataAndResults.remove_sheet(delete)
 
-        Sheet3.column_dimensions['A'].width = 30.0
-        Sheet3.column_dimensions['B'].width = 30.0
-        Sheet3.column_dimensions['C'].width = 20.0
-        Sheet3.column_dimensions['D'].width = 20.0
+        Sheet2 = DataAndResults.create_sheet("TestResultsForFacebookLogin")
 
-        Sheet3["A1"].alignment = position_left()
-        Sheet3["A1"].font = styleBold()
-        Sheet3["A1"] = "username"
-        Sheet3["B1"].alignment = position_left()
-        Sheet3["B1"].font = styleBold()
-        Sheet3["B1"] = "password"
-        Sheet3["C1"].alignment = position_center()
-        Sheet3["C1"].font = styleBold()
-        Sheet3["C1"] = "correctness"
-        Sheet3["D1"].font = styleBold()
-        Sheet3["D1"].alignment = position_center()
-        Sheet3["D1"] = "test results"
+        Sheet2.column_dimensions['A'].width = 30.0
+        Sheet2.column_dimensions['B'].width = 30.0
+        Sheet2.column_dimensions['C'].width = 20.0
+        Sheet2.column_dimensions['D'].width = 20.0
+
+        Sheet2["A1"].alignment = position_left()
+        Sheet2["A1"].font = styleBold()
+        Sheet2["A1"] = "username"
+        Sheet2["B1"].alignment = position_left()
+        Sheet2["B1"].font = styleBold()
+        Sheet2["B1"] = "password"
+        Sheet2["C1"].alignment = position_center()
+        Sheet2["C1"].font = styleBold()
+        Sheet2["C1"] = "correctness"
+        Sheet2["D1"].font = styleBold()
+        Sheet2["D1"].alignment = position_center()
+        Sheet2["D1"] = "test results"
 
         number = 2
 
@@ -47,14 +43,43 @@ class Format():
             password = value[1]
             correctness = value[2]
 
-            Sheet3["A" + str(number)].alignment = position_left()
-            Sheet3["A" + str(number)] = username
-            Sheet3["B" + str(number)].alignment = position_left()
-            Sheet3["B" + str(number)] = password
-            Sheet3["C" + str(number)].alignment = position_center()
-            Sheet3["C" + str(number)] = correctness
+            Sheet2["A" + str(number)].alignment = position_left()
+            Sheet2["A" + str(number)] = username
+            Sheet2["B" + str(number)].alignment = position_left()
+            Sheet2["B" + str(number)] = password
+            Sheet2["C" + str(number)].alignment = position_center()
+            Sheet2["C" + str(number)] = correctness
 
             number+=1
 
-        UserPass.save(self.file)
+        DataAndResults.save(self.file)
 
+class Format_other_test_results():
+
+    def __init__(self,file):
+        self.file = file
+
+    def formatTable(self):
+        filePath = os.path.join(os.getcwd(), self.file)
+        DataAndResults = load_workbook(filePath)
+
+        delete = DataAndResults.get_sheet_by_name("other_test_results")
+        DataAndResults.remove_sheet(delete)
+
+        Sheet3 = DataAndResults.create_sheet("other_test_results")
+
+        Sheet3.column_dimensions['A'].width = 30.0
+        Sheet3.column_dimensions['B'].width = 30.0
+        Sheet3.column_dimensions['C'].width = 20.0
+
+        Sheet3["A1"].alignment = position_left()
+        Sheet3["A1"].font = styleBold()
+        Sheet3["A1"] = "test name"
+        Sheet3["B1"].alignment = position_center()
+        Sheet3["B1"].font = styleBold()
+        Sheet3["B1"] = "class name"
+        Sheet3["C1"].alignment = position_center()
+        Sheet3["C1"].font = styleBold()
+        Sheet3["C1"] = "test results"
+
+        DataAndResults.save(self.file)
